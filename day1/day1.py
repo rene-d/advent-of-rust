885837fd5ca1c84691d616a9c1900e693ac3dd71
rene-d <10288093+rene-d@users.noreply.github.com>
2021-12-14 15:17:42 +0100

    Create day1.py

---
+++ day1/day1.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+# read the input
+data = open("input.txt").readlines()
+data = list(map(int, data))
+
+# --- Day 1: Sonar Sweep --- part one
+n = 0
+for i, j in zip(data, data[1:]):
+    if i < j:
+        n += 1
+print(n)
+
+# --- Day 1: Sonar Sweep --- part two
+data = list(i + j + k for i, j, k in zip(data, data[1:], data[2:]))
+n = 0
+for i, j in zip(data, data[1:]):
+    if i < j:
+        n += 1
+print(n)
