69cbc60b6a816becf76962883c1f9364533d2bb8
rene-d <rene-d@users.noreply.github.com>
2021-12-17 18:32:30 +0100

    day 17

--- day17/day17.rs
+++ day17/day17.rs
@@ -15,8 +15,8 @@ fn main() {
     let mut part1 = 0;
     let mut part2 = 0;
 
-    for vx0  in 0..1000 {
-        for vy0  in -200..1000 {
+    for vx0 in 0..1000 {
+        for vy0 in -200..1000 {
             let mut hit = false;
             let mut y_max = 0;
 
@@ -27,7 +27,6 @@ fn main() {
             let mut vy = vy0;
 
             for _ in 0..1000 {
-
                 x += vx; // probe's x position increases by its x velocity
                 y += vy; // probe's y position increases by its y velocity
 
@@ -35,9 +34,11 @@ fn main() {
                     y_max = y; // the highest y position
                 }
 
-                if vx > 0 {  // the probe's x velocity changes by 1 toward the value 0
+                if vx > 0 {
+                    // the probe's x velocity changes by 1 toward the value 0
                     vx -= 1;
-                } else if vx < 0 { // increases by 1 if it is less than 0
+                } else if vx < 0 {
+                    // increases by 1 if it is less than 0
                     vx += 1;
                 } else {
                     // does not change if it is already 0
@@ -48,7 +49,6 @@ fn main() {
                 if target_x1 <= x && x <= target_x2 && target_y1 <= y && y <= target_y2 {
                     hit = true;
                 }
-
             }
 
             if hit {
