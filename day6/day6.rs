c590048389dd73938f14351a6ed7ad1b937ae00a
rene-d <rene-d@users.noreply.github.com>
2021-12-19 03:15:08 +0100

    rust

--- day6/day6.rs
+++ day6/day6.rs
@@ -6,6 +6,7 @@ use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 use structopt::StructOpt;
+use std::fs;
 
 /// parse command line arguments
 #[derive(StructOpt)]
@@ -18,10 +19,11 @@ struct Cli {
 fn main() {
     let args = Cli::from_args();
     println!("reading data from: {}", args.path.display());
-    let data = load_data(args.path);
+
+    let data = fs::read_to_string(args.path).unwrap();
 
     let mut timers = [0u64; 9];
-    for timer in data[0].split(",").map(|s| s.parse::<u64>().unwrap()) {
+    for timer in data.split(",").map(|s| s.parse::<u64>().unwrap()) {
         timers[timer as usize] += 1;
     }
 
@@ -46,27 +48,4 @@ fn lanterfishes(initial: &[u64; 9], days: u64) {
     }
 
     println!("{}", timers.iter().sum::<u64>());
-}
-
-// The output is wrapped in a Result to allow matching on errors
-// Returns an Iterator to the Reader of the lines of the file.
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where
-    P: AsRef<Path>,
-{
-    let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
-}
-
-/// load data from file
-fn load_data(path: std::path::PathBuf) -> Vec<String> {
-    let mut data = vec![];
-    if let Ok(lines) = read_lines(path) {
-        for line in lines {
-            if let Ok(bits) = line {
-                data.push(bits);
-            }
-        }
-    }
-    data
 }
