ee7f8ee76bffe86f2ced27bfece62e0631ffb421
Pierre Gagelin <pierre.gagelin@gmail.com>
2022-01-17 20:16:50 +0100

    Fixed clippy without encapsulation

--- 2021/day25/day25.rs
+++ 2021/day25/day25.rs
@@ -13,10 +13,6 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
-struct Herd {
-    grid: Vec<Vec<char>>,
-}
-
 /// main function
 fn main() {
     let args = Cli::from_args();
@@ -26,45 +22,43 @@ fn main() {
 
     let nx = data[0].len();
     let ny = data.len();
-    let mut herd = Herd {
-        grid: vec![vec!['.'; nx]; ny],
-    };
+    let mut grid = vec![vec!['.'; nx]; ny];
 
     // load the grid
     for (y, line) in data.iter().enumerate() {
         for (x, col) in line.chars().enumerate() {
-            herd.grid[y][x] = col;
+            grid[y][x] = col;
         }
     }
 
     // move the sea cucumbers
     let mut step = 1;
-    while do_move(&mut herd) {
+    while do_move(&mut grid) {
         step += 1;
     }
     println!("{}", step);
 }
 
-fn do_move(herd: &mut Herd) -> bool {
+fn do_move(grid: &mut [Vec<char>]) -> bool {
     let mut moved = false;
 
-    let nx = herd.grid[0].len();
-    let ny = herd.grid.len();
+    let nx = grid[0].len();
+    let ny = grid.len();
 
     // don't move blocked sea cucumbers
     for y in 0..ny {
         for x in 0..nx {
-            if herd.grid[y][x] == '>' && herd.grid[y][x] == herd.grid[y][(x + 1) % nx] {
-                herd.grid[y][x] = 'H';
+            if grid[y][x] == '>' && grid[y][x] == grid[y][(x + 1) % nx] {
+                grid[y][x] = 'H';
             }
-            if herd.grid[y][x] == 'v' && herd.grid[y][x] == herd.grid[(y + 1) % ny][x] {
-                herd.grid[y][x] = 'V';
+            if grid[y][x] == 'v' && grid[y][x] == grid[(y + 1) % ny][x] {
+                grid[y][x] = 'V';
             }
         }
     }
 
     // During a single step, the east-facing herd moves first,
-    for line in &mut herd.grid {
+    for line in grid.iter_mut() {
         for x in 0..nx {
             if line[x] == '>' && line[(x + 1) % nx] == '.' {
                 line[(x + 1) % nx] = 'H';
@@ -77,17 +71,17 @@ fn do_move(herd: &mut Herd) -> bool {
     // then the south-facing herd moves.
     for y in 0..ny {
         for x in 0..nx {
-            let c = herd.grid[y][x];
-            if c == 'v' && herd.grid[(y + 1) % ny][x] == '.' {
-                herd.grid[(y + 1) % ny][x] = 'V';
-                herd.grid[y][x] = '.';
+            let c = grid[y][x];
+            if c == 'v' && grid[(y + 1) % ny][x] == '.' {
+                grid[(y + 1) % ny][x] = 'V';
+                grid[y][x] = '.';
                 moved = true;
             }
         }
     }
 
     // restore blocked and moving sea cucumbers
-    for line in &mut herd.grid {
+    for line in grid {
         for val in line {
             match val {
                 'H' => *val = '>',
