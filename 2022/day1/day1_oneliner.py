ecf8617a7f71410e8ca4dffd7ec253afb674840c
rene-d <10288093+rene-d@users.noreply.github.com>
2022-12-01 17:08:12 +0100

    Update day1_oneliner.py

--- 2022/day1/day1_oneliner.py
+++ 2022/day1/day1_oneliner.py
@@ -1,5 +1,3 @@
 #!/usr/bin/env python3
 
-a = [sum(map(int, i.split())) for i in open("input.txt").read().split("\n\n")]
-print("part1:", max(a))
-print("part2:", sum(sorted(a)[-3:]))
+list(filter(lambda a: print(max(a), sum(sorted(a)[-3:])), ([sum(map(int, i.split())) for i in open("input.txt").read().split("\n\n")],)))
