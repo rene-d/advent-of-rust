e27ad51ae6dd077c8d5985125ac65a5a9faf995c
rene-d <rene-d@users.noreply.github.com>
2022-12-01 07:35:41 +0100

    2022 day1

---
+++ 2022/day1/day1.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+lines = open("input.txt").readlines()
+lines.append("")
+
+
+energy = 0
+reeinders = []
+for line in lines:
+    line = line.strip()
+    if not line:
+        reeinders.append(energy)
+        energy = 0
+    else:
+        energy += int(line)
+
+# part one
+print(max(reeinders))
+
+# part two
+reeinders = sorted(reeinders, reverse=True)
+print(reeinders[0] + reeinders[1] + reeinders[2])
