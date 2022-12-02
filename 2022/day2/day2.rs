c1528060ccb5f8f280274dbf238cbcb8794792a3
rene-d <rene-d@users.noreply.github.com>
2022-12-02 08:01:04 +0100

    test

--- 2022/day2/day2.rs
+++ 2022/day2/day2.rs
@@ -15,74 +15,96 @@ pub const NEED_TO_LOOSE: u32 = 1;
 pub const NEED_TO_DRAW: u32 = 2;
 pub const NEED_TO_WIN: u32 = 3;
 
-fn main() {
-    let data = std::fs::read_to_string("input.txt").unwrap();
-    let lines = data.split('\n').collect::<Vec<_>>();
+struct Puzzle {
+    part1: u32,
+    part2: u32,
+}
 
-    let mut part1 = 0;
-    let mut part2 = 0;
+impl Puzzle {
+    fn new() -> Puzzle {
+        Puzzle { part1: 0, part2: 0 }
+    }
 
-    for line in lines {
-        if line.is_empty() {
-            continue;
-        }
-        let ab = line.split(' ').collect::<Vec<_>>();
+    fn solve(&mut self, path: &str) {
+        let data = std::fs::read_to_string(path).unwrap();
+        let lines = data.split('\n').collect::<Vec<_>>();
 
-        let opponent = match *ab.first().unwrap() {
-            "A" => ROCK,
-            "B" => PAPER,
-            "C" => SCISSORS,
-            _ => panic!("bad input"),
-        };
-
-        // part 1
-        let me = match *ab.get(1).unwrap() {
-            "X" => ROCK,
-            "Y" => PAPER,
-            "Z" => SCISSORS,
-            _ => panic!("bad input"),
-        };
-
-        if opponent == me {
-            part1 += opponent + SCORE_DRAW;
-        } else if (opponent == ROCK && me == SCISSORS)
-            || (opponent == SCISSORS && me == PAPER)
-            || (opponent == PAPER && me == ROCK)
-        {
-            part1 += me + SCORE_LOOSE;
-        } else {
-            part1 += me + SCORE_WIN;
-        }
-
-        // part 2
-        let need_to = match *ab.get(1).unwrap() {
-            "X" => NEED_TO_LOOSE,
-            "Y" => NEED_TO_DRAW,
-            "Z" => NEED_TO_WIN,
-            _ => panic!("bad input"),
-        };
-
-        if need_to == NEED_TO_DRAW {
-            part2 += opponent + SCORE_DRAW;
-        } else if need_to == NEED_TO_LOOSE {
-            if opponent == ROCK {
-                part2 += SCISSORS + SCORE_LOOSE;
-            } else if opponent == PAPER {
-                part2 += ROCK + SCORE_LOOSE;
-            } else if opponent == SCISSORS {
-                part2 += PAPER + SCORE_LOOSE;
+        for line in lines {
+            if line.is_empty() {
+                continue;
             }
-        } else {
-            if opponent == ROCK {
-                part2 += PAPER + SCORE_WIN;
-            } else if opponent == PAPER {
-                part2 += SCISSORS + SCORE_WIN;
-            } else if opponent == SCISSORS {
-                part2 += ROCK + SCORE_WIN;
+            let turn = line.split(' ').collect::<Vec<_>>();
+
+            let opponent = match *turn.first().unwrap() {
+                "A" => ROCK,
+                "B" => PAPER,
+                "C" => SCISSORS,
+                _ => panic!("bad input"),
+            };
+
+            // part 1
+            let me = match *turn.get(1).unwrap() {
+                "X" => ROCK,
+                "Y" => PAPER,
+                "Z" => SCISSORS,
+                _ => panic!("bad input"),
+            };
+
+            if opponent == me {
+                self.part1 += opponent + SCORE_DRAW;
+            } else if (opponent == ROCK && me == SCISSORS)
+                || (opponent == SCISSORS && me == PAPER)
+                || (opponent == PAPER && me == ROCK)
+            {
+                self.part1 += me + SCORE_LOOSE;
+            } else {
+                self.part1 += me + SCORE_WIN;
+            }
+
+            // part 2
+            let need_to = match *turn.get(1).unwrap() {
+                "X" => NEED_TO_LOOSE,
+                "Y" => NEED_TO_DRAW,
+                "Z" => NEED_TO_WIN,
+                _ => panic!("bad input"),
+            };
+
+            if need_to == NEED_TO_DRAW {
+                self.part2 += opponent + SCORE_DRAW;
+            } else if need_to == NEED_TO_LOOSE {
+                if opponent == ROCK {
+                    self.part2 += SCISSORS + SCORE_LOOSE;
+                } else if opponent == PAPER {
+                    self.part2 += ROCK + SCORE_LOOSE;
+                } else if opponent == SCISSORS {
+                    self.part2 += PAPER + SCORE_LOOSE;
+                }
+            } else {
+                if opponent == ROCK {
+                    self.part2 += PAPER + SCORE_WIN;
+                } else if opponent == PAPER {
+                    self.part2 += SCISSORS + SCORE_WIN;
+                } else if opponent == SCISSORS {
+                    self.part2 += ROCK + SCORE_WIN;
+                }
             }
         }
     }
+}
 
-    println!("{}", part1);
-    println!("{}", part2);
+/// Solve the puzzle with the user input
+fn main() {
+    let mut puzzle = Puzzle::new();
+    puzzle.solve("input.txt");
+    println!("{}", puzzle.part1);
+    println!("{}", puzzle.part2);
+}
+
+/// Test from puzzle input
+#[test]
+fn test_puzzle() {
+    let mut puzzle = Puzzle::new();
+    puzzle.solve("test.txt");
+    assert_eq!(puzzle.part1, 15);
+    assert_eq!(puzzle.part2, 12);
 }
