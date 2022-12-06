ebc8da34c134f0119585d9a59448ce808dd211a1
rene-d <rene-d@users.noreply.github.com>
2022-12-06 15:16:00 +0100

    almost oneliner

--- 2022/day6/day6.py
+++ 2022/day6/day6.py
@@ -1,9 +1,4 @@
 #!/usr/bin/env python3
 
 data = open("input.txt").read()
-
-for length in (4, 14):
-    for i in range(len(data) - length):
-        if len(set(data[i : i + length])) == length:
-            print(i + length)
-            break
+print(list(min(i + length for i in range(len(data) - length) if len(set(data[i : i + length])) == length) for length in (4, 14)))
