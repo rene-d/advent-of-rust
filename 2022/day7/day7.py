663ae774c5b66db2317743d59b85faef4360fde2
rene-d <10288093+rene-d@users.noreply.github.com>
2022-12-08 22:39:54 +0100

    2022 day7 (#21)
    * Added solution to puzzle 2021/day14
    
    * input
    
    * day7 in python
    
    * Update day7.rs
    
    * clap 4
    
    * cargo fmt
    
    Co-authored-by: PierreGagelin <pierre.gagelin@gmail.com>
    Co-authored-by: rene-d <rene-d@users.noreply.github.com>

---
+++ 2022/day7/day7.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+from pathlib import Path
+import sys
+
+filename = "test.txt" if len(sys.argv) > 1 and sys.argv[1] == "-t" else "input.txt"
+
+path = Path("/")
+dirs = set()
+files = {}
+
+for line in open(filename):
+    line = line.strip()
+
+    if line == "$ cd /":
+        path = Path("/")
+    elif line == "$ cd ..":
+        path = path.parent
+    elif line.startswith("$ cd "):
+        assert "/" not in line[5:]
+        assert len(line) > 5
+        path /= line[5:]
+    elif line == "$ ls":
+        pass
+    else:
+        if line.startswith("dir "):
+            pass
+        else:
+            size, file = line.split()
+            size = int(size)
+
+            files[path / file] = size
+    dirs.add(path)
+
+dirs_size=[]
+total=0
+
+# part one (and computing for part two)
+part1 = 0
+for dir in dirs:
+    s = 0
+    for file_path, file_size in files.items():
+        if file_path.as_posix().startswith(dir.as_posix()):
+            s += file_size
+    if s <= 100000:
+        part1 += s
+
+    dirs_size.append(s)
+    if dir.as_posix() == "/":
+        total = s
+
+print(part1)
+
+# part two
+for part2 in sorted(dirs_size):
+    if total - part2 + 30000000 <= 70000000:
+        print(part2)
+        break
+
