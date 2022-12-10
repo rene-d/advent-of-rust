965ba1a74145d57efca5f97a90271cb7ad4a53d1
rene-d <rene-d@users.noreply.github.com>
2022-12-10 11:49:03 +0100

    better shell

--- 2022/check.sh
+++ 2022/check.sh
@@ -6,7 +6,6 @@ cargo fmt
 cargo clippy
 cargo test
 cargo build --release
-for i in */Cargo.toml ; do
-    echo $(dirname $i) | cut -b4-
-done | sort -n | xargs -n1 -I % cargo run --offline --release --bin day% -- day%/input.txt
+days=($(for i in */Cargo.toml ; do echo $(dirname $i) | cut -b4- ; done | sort -n))
+echo ${days[@]} | xargs -n1 -I % cargo run --offline --release --bin day% -- day%/input.txt
 
