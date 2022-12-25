0ccdbad572b5996236186b02eb6bec4dd58f60fa
rene-d <rene-d@users.noreply.github.com>
2022-12-25 08:56:08 +0100

    🎄🎅🎁

---
+++ 2022/day25/day25.rs
@@ -0,0 +1,90 @@
+//! [Day 25: Full of Hot Air](https://adventofcode.com/2022/day/25)
+
+use clap::Parser;
+
+#[derive(Parser)]
+struct Args {
+    /// Puzzle input
+    #[arg(default_value = "input.txt")]
+    path: String,
+}
+
+fn from_snafu(s: &str) -> i64 {
+    let mut n = 0;
+    let mut pow = 1;
+    for ch in s.chars().rev() {
+        n += pow
+            * match ch {
+                '2' => 2,
+                '1' => 1,
+                '0' => 0,
+                '-' => -1,
+                '=' => -2,
+                _ => panic!("unknown digit '{}'", ch),
+            };
+        pow *= 5;
+    }
+    n
+}
+
+fn to_snafu(mut n: i64) -> String {
+    let mut digits = vec![];
+    loop {
+        let ch = ['=', '-', '0', '1', '2'][(n + 2).rem_euclid(5) as usize];
+        digits.push(ch);
+        n = (n + 2).div_euclid(5);
+        if n == 0 {
+            break;
+        }
+    }
+
+    digits.iter().rev().collect::<String>()
+}
+
+struct Puzzle {
+    sum_of_numbers: i64,
+}
+
+impl Puzzle {
+    fn new() -> Self {
+        Self { sum_of_numbers: 0 }
+    }
+
+    /// Loads data from input (one line)
+    fn configure(&mut self, path: &str) {
+        let data = std::fs::read_to_string(path).unwrap();
+
+        self.sum_of_numbers = data.lines().map(from_snafu).sum();
+    }
+
+    // Solves part one
+    fn part1(&self) -> String {
+        to_snafu(self.sum_of_numbers)
+    }
+}
+
+/// main function
+fn main() {
+    let args = Args::parse();
+    let mut puzzle = Puzzle::new();
+    puzzle.configure(&args.path);
+    println!("{}", puzzle.part1());
+}
+
+#[test]
+fn test01() {
+    let mut puzzle = Puzzle::new();
+    puzzle.configure("test.txt");
+    assert_eq!(puzzle.sum_of_numbers, 4890);
+    assert_eq!(puzzle.part1(), "2=-1=0");
+}
+
+#[test]
+fn test_from_snafu() {
+    assert_eq!(from_snafu("2=-01"), 976);
+}
+
+#[test]
+fn test_to_snafu() {
+    assert_eq!(to_snafu(976), "2=-01");
+}
