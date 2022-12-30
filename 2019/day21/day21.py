378070a5f3a5d02eb04bb697fabab74fcc901dd5
rene-d <rene-d@users.noreply.github.com>
2022-12-30 19:56:19 +0100

    day21 2019

---
+++ 2019/day21/day21.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+# https://adventofcode.com/2019/day/21
+
+from pathlib import Path
+import sys
+from copy import deepcopy
+from collections import defaultdict, deque
+import re
+
+filename = "test.txt" if len(sys.argv) > 1 and sys.argv[1] == "-t" else "input.txt"
+data = Path(filename).read_text()
+lines = data.splitlines()
+
+
+sys.path.append("..")
+from intcode.Intcode import Computer
+
+
+filename = sys.argv[1] if len(sys.argv) > 1 else "input.txt"
+software = Path(filename).read_text()
+
+droid = Computer()
+droid.load(software)
+
+
+def run_script(script):
+    droid.flush_io()
+    droid.input.extend(map(ord, script))
+    droid.run()
+
+    res = droid.output[-1]
+    if res == 10:
+        print("error")
+    else:
+        print(res)
+
+
+# J = not A or not (C and D)
+run_script(
+    """\
+NOT A J
+NOT C T
+AND D T
+OR T J
+WALK
+"""
+)
+
+
+# J = not A or not (C and D)
+run_script(
+    """\
+NOT B J
+NOT C T
+OR T J
+AND D J
+AND H J
+NOT A T
+OR T J
+RUN
+"""
+)
