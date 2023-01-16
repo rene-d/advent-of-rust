f9fb9348f637a5dafeb923d23577ced76514b4ef
rene-d <rene-d@users.noreply.github.com>
2023-01-16 23:23:11 +0100

    fix test

--- 2022/day7/day7.rs
+++ 2022/day7/day7.rs
@@ -1,10 +1,9 @@
 //! [Day 7: No Space Left On Device](https://adventofcode.com/2022/day/7)
 
-// I keep "// ignore" for completude
 #![allow(clippy::if_same_then_else)]
 
 use clap::Parser;
-use std::collections::{HashMap, HashSet};
+use std::collections::HashMap;
 use std::path::PathBuf;
 
 #[derive(Parser)]
@@ -16,66 +15,79 @@ struct Args {
 
 struct Puzzle {
     // Puzzle input
-    dirs_size: HashMap<String, usize>,
+    total_dir_size: HashMap<String, usize>,
 }
 
 impl Puzzle {
     fn new() -> Self {
         Self {
             // data: String::new(),
-            dirs_size: HashMap::new(),
+            total_dir_size: HashMap::new(),
         }
     }
 
     /// Loads data from input (one line)
     fn configure(&mut self, path: &str) {
         let data = std::fs::read_to_string(path).unwrap();
-        let lines = data.split('\n').collect::<Vec<_>>();
+        let lines = data.lines().collect::<Vec<_>>();
 
+        let mut dir_size = HashMap::new();
         let mut current_path = PathBuf::from("/");
-        let mut dirs = HashSet::new();
-        let mut files = HashMap::new();
+
+        dir_size.entry("/".to_string()).or_insert(0);
 
         for line in lines {
             if line == "$ cd /" {
                 current_path = PathBuf::from("/");
             } else if line == "$ cd .." {
+                assert_ne!(current_path.as_path().as_os_str().len(), 1);
                 current_path.pop();
             } else if let Some(dir) = line.strip_prefix("$ cd ") {
                 current_path.push(dir);
+
+                let key = current_path.as_path().to_str().unwrap().to_string();
+                dir_size.entry(key).or_insert(0);
             } else if line == "$ ls" {
-                // ignore
+                //  nothing to do
             } else if line.starts_with("dir ") {
-                // ignore
+                //  nothing to do
             } else if !line.is_empty() {
+                // <size> <filename>
                 let mut info = line.split(' ');
                 let size = info.next().unwrap().parse::<usize>().unwrap();
-                let name = info.next().unwrap();
+                // let filename = info.next().unwrap();
 
-                current_path.push(name);
-                files.insert(current_path.as_path().to_str().unwrap().to_string(), size);
-                current_path.pop();
+                let key = current_path.as_path().to_str().unwrap().to_string();
+                *dir_size.get_mut(&key).unwrap() += size;
             }
-
-            dirs.insert(current_path.as_path().to_str().unwrap().to_string());
         }
 
-        for dir in dirs {
-            let mut dir_size = 0;
-            for file in &files {
-                if file.0.starts_with(&dir) {
-                    dir_size += file.1;
-                }
-            }
-            self.dirs_size.insert(dir, dir_size);
+        for dir in dir_size.keys() {
+            let total = dir_size
+                .iter()
+                .map(|(x, size)| {
+                    if dir.len() == 1
+                        || x.starts_with(dir)
+                            && (x.len() == dir.len() || {
+                                x.chars().nth(dir.len()).unwrap() == std::path::MAIN_SEPARATOR
+                            })
+                    {
+                        size
+                    } else {
+                        &0
+                    }
+                })
+                .sum();
+
+            self.total_dir_size.insert(dir.clone(), total);
         }
     }
 
     // Solves part one
     fn part1(&self) -> usize {
         let mut result = 0;
-        for dir in &self.dirs_size {
-            if dir.1 <= &100000 {
+        for dir in &self.total_dir_size {
+            if dir.1 <= &100_000 {
                 result += dir.1;
             }
         }
@@ -84,13 +96,10 @@ impl Puzzle {
 
     // Solve part two
     fn part2(&self) -> usize {
-        let total = self.dirs_size.get("/").unwrap();
+        let total = self.total_dir_size.get("/").unwrap();
 
-        let mut sizes = vec![];
-        for dir in &self.dirs_size {
-            sizes.push(dir.1);
-        }
-        sizes.sort();
+        let mut sizes = self.total_dir_size.values().collect::<Vec<_>>();
+        sizes.sort_unstable();
 
         for size in sizes {
             if total - size + 30000000 <= 70000000 {
