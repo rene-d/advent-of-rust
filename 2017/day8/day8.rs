343b82adc348f0f1b62f40727507d1fe92e9e491
rene-d <rene-d@users.noreply.github.com>
2023-12-02 22:51:24 +0100

    2017

---
+++ 2017/day8/day8.rs
@@ -0,0 +1,128 @@
+//! [Day 8: I Heard You Like Registers](https://adventofcode.com/2017/day/8)
+
+use std::collections::HashMap;
+
+use clap::Parser as ClapParser;
+use pest::Parser;
+use pest_derive::Parser as PestParser;
+
+#[derive(ClapParser)]
+struct Args {
+    /// Puzzle input
+    #[arg(default_value = "input.txt")]
+    path: String,
+}
+
+#[derive(PestParser, Debug)]
+#[grammar = "day8.pest"]
+struct MyParser;
+
+struct Puzzle {
+    registers: HashMap<String, i32>,
+
+    last_max: i32,  // i.e. part 1
+    value_max: i32, // i.e. part 2
+}
+
+impl Puzzle {
+    fn new() -> Puzzle {
+        Puzzle {
+            registers: HashMap::new(),
+            last_max: 0,
+            value_max: 0,
+        }
+    }
+
+    /// Get the puzzle input.
+    fn configure(&mut self, path: &str) {
+        let data = std::fs::read_to_string(path).unwrap();
+
+        self.parse(&data);
+    }
+
+    /// Parse the input.
+    fn parse_line(&mut self, line: &str) {
+        let mut a = MyParser::parse(Rule::instr, line)
+            .unwrap()
+            .next()
+            .unwrap()
+            .into_inner();
+
+        let target = a.next().unwrap().as_str();
+        let oper = a.next().unwrap().as_rule();
+        let value = a.next().unwrap().as_str().parse::<i32>().unwrap();
+        let lhs = a.next().unwrap().as_str();
+        let cmp = a.next().unwrap().as_rule();
+        let rhs = a.next().unwrap().as_str().parse::<i32>().unwrap();
+
+        let lhs = self.registers.get(lhs).unwrap_or(&0);
+
+        let ok = match cmp {
+            Rule::equal => lhs == &rhs,
+            Rule::different => lhs != &rhs,
+            Rule::greater => lhs > &rhs,
+            Rule::greater_or_equal => lhs >= &rhs,
+            Rule::less => lhs < &rhs,
+            Rule::less_or_equal => lhs <= &rhs,
+            _ => panic!(),
+        };
+
+        if ok {
+            let r = self.registers.entry(target.to_string()).or_insert(0);
+
+            match oper {
+                Rule::inc => *r += value,
+                Rule::dec => *r -= value,
+                _ => panic!(),
+            }
+
+            let max = *self.registers.values().max().unwrap();
+            self.last_max = max;
+            self.value_max = self.value_max.max(self.last_max);
+        }
+    }
+
+    fn parse(&mut self, data: &str) {
+        for line in data.lines() {
+            self.parse_line(line);
+        }
+    }
+
+    /// Solve part one.
+    fn part1(&self) -> i32 {
+        self.last_max
+    }
+
+    /// Solve part two.
+    fn part2(&self) -> i32 {
+        self.value_max
+    }
+}
+
+/// Test from puzzle input
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test01() {
+        let mut puzzle = Puzzle::new();
+        puzzle.configure("test.txt");
+        assert_eq!(puzzle.part1(), 1);
+    }
+
+    #[test]
+    fn test02() {
+        let mut puzzle = Puzzle::new();
+        puzzle.configure("test.txt");
+        assert_eq!(puzzle.part2(), 10);
+    }
+}
+
+fn main() {
+    let args = Args::parse();
+    let mut puzzle = Puzzle::new();
+    puzzle.configure(args.path.as_str());
+    println!("{}", puzzle.part1());
+    println!("{}", puzzle.part2());
+}
