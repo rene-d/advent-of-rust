16ce21ca388ccbc376f725cfe2699a7f48539208
rene-d <rene-d@users.noreply.github.com>
2023-12-02 08:47:56 +0100

    2017

---
+++ 2017/day5/day5.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# https://adventofcode.com/2017/day/5
+
+import itertools
+import math
+import re
+import sys
+import time
+from collections import defaultdict, deque, namedtuple
+from copy import deepcopy
+from functools import reduce
+from pathlib import Path
+
+filename = ("test.txt" if sys.argv[1] == "-t" else sys.argv[1]) if len(sys.argv) > 1 else "input.txt"
+data = Path(filename).read_text().strip()
+
+maze = list(map(int, data.splitlines()))
+i = 0
+n = 0
+while 0 <= i < len(maze):
+    offset = maze[i]
+    maze[i] += 1
+    i += offset
+    n += 1
+print(n)
+
+maze = list(map(int, data.splitlines()))
+i = 0
+n = 0
+while 0 <= i < len(maze):
+    offset = maze[i]
+    if offset >= 3:
+        maze[i] -= 1
+    else:
+        maze[i] += 1
+    i += offset
+    n += 1
+print(n)
