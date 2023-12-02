7853dd343dbe4f122d970cb41281fb0ed57074c3
rene-d <rene-d@users.noreply.github.com>
2023-12-02 22:51:24 +0100

    2017

---
+++ 2017/day8/day8.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+# https://adventofcode.com/2017/day/8
+
+from pathlib import Path
+from copy import deepcopy
+from collections import defaultdict, deque, namedtuple
+import sys, re, math, itertools, time
+from functools import reduce
+import re
+
+
+filename = ("test.txt" if sys.argv[1] == "-t" else sys.argv[1]) if len(sys.argv) > 1 else "input.txt"
+data = Path(filename).read_text().strip()
+lines = data.splitlines()
+
+registers = defaultdict(lambda: 0)
+
+part2 = 0
+
+for line in lines:
+    m = re.match(
+        r"^(?P<target>\w+) (?P<op>(inc|dec)) (?P<value>-?\d+)"
+        r" if (?P<register>\w+) (?P<condition>(==|<=|<|!=|>=|>)) (?P<compare>-?\d+)",
+        line,
+    )
+
+    compare_register = registers[m["register"]]
+    compare_value = int(m["compare"])
+
+    match m["condition"]:
+        case "==":
+            result = compare_register == compare_value
+        case "!=":
+            result = compare_register != compare_value
+        case ">":
+            result = compare_register > compare_value
+        case ">=":
+            result = compare_register >= compare_value
+        case "<":
+            result = compare_register < compare_value
+        case "<=":
+            result = compare_register <= compare_value
+        case _:
+            raise ValueError
+
+    if result:
+        match m["op"]:
+            case "inc":
+                registers[m["target"]] += int(m["value"])
+            case "dec":
+                registers[m["target"]] -= int(m["value"])
+            case _:
+                raise ValueError
+
+    part1 = max(registers.values())
+    part2 = max(part2, part1)
+
+print(part1)
+print(part2)
--- scripts/puzzle.sh
+++ scripts/puzzle.sh
@@ -28,6 +28,7 @@ from copy import deepcopy
 from collections import defaultdict, deque, namedtuple
 import sys, re, math, itertools, time
 from functools import reduce
+import re
 
 filename = ("test.txt" if sys.argv[1] == "-t" else sys.argv[1]) if len(sys.argv) > 1 else "input.txt"
 data = Path(filename).read_text().strip()
@@ -36,5 +37,92 @@ EOF
     chmod a+x day$1.py
 fi
 
-code .
-code day$1.py
+if [ ! -f day$1.rs ]; then
+    cat <<EOF >day$1.rs
+//! [Day $1: xxx](https://adventofcode.com/$year/day/$1)
+
+use clap::Parser;
+
+#[derive(Parser)]
+struct Args {
+    /// Puzzle input
+    #[arg(default_value = "input.txt")]
+    path: String,
+}
+
+struct Puzzle {
+    data: String,
+}
+
+impl Puzzle {
+    fn new() -> Puzzle {
+        Puzzle {
+            data: "".to_string(),
+        }
+    }
+
+    /// Get the puzzle input.
+    fn configure(&mut self, path: &str) {
+        let data = std::fs::read_to_string(path).unwrap();
+
+        self.data = data;
+    }
+
+    /// Solve part one.
+    fn part1(&self) -> u32 {
+        0
+    }
+
+    /// Solve part two.
+    fn part2(&self) -> u32 {
+        0
+    }
+}
+
+/// Test from puzzle input
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test01() {
+        let mut puzzle = Puzzle::new();
+        puzzle.configure("test.txt");
+        assert_eq!(puzzle.part1(), 0);
+    }
+
+    #[test]
+    fn test02() {
+        let mut puzzle = Puzzle::new();
+        puzzle.configure("test.txt");
+        assert_eq!(puzzle.part2(), 0);
+    }
+}
+
+fn main() {
+    let args = Args::parse();
+    let mut puzzle = Puzzle::new();
+    puzzle.configure(args.path.as_str());
+    println!("{}", puzzle.part1());
+    println!("{}", puzzle.part2());
+}
+EOF
+fi
+
+if [ ! -f Cargo.toml ]; then
+    cat <<EOF >Cargo.toml
+[package]
+name = "day$1"
+version = "0.1.0"
+edition = "2021"
+
+[dependencies]
+clap = { version = "4.*", features = ["derive"] }
+
+[[bin]]
+name = "day$1"
+path = "day$1.rs"
+EOF
+fi
+
+code --add . day$1.py
