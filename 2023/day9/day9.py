9e22f7621fa4ff24d06eb4bbb989abbf684b7394
rene-d <rene-d@users.noreply.github.com>
2023-12-09 08:19:54 +0100

    day 9

---
+++ 2023/day9/day9.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# https://adventofcode.com/2023/day/9
+
+import sys
+from pathlib import Path
+from functools import reduce
+
+filename = ("test.txt" if sys.argv[1] == "-t" else sys.argv[1]) if len(sys.argv) > 1 else "input.txt"
+data = Path(filename).read_text().strip()
+lines = data.splitlines()
+
+part1 = 0
+part2 = 0
+
+for line in lines:
+    line = list(map(int, line.split()))
+    diffs = [line]
+
+    while True:
+        history = []
+        for a, b in zip(line, line[1:]):
+            history.append(b - a)
+
+        if set(history) == set([0]):
+            break
+
+        diffs.append(history)
+        line = history
+
+    right = sum(d[-1] for d in diffs)
+    left = reduce(lambda left, d: d[0] - left, reversed(diffs), 0)
+
+    part1 += right
+    part2 += left
+
+print(part1)
+print(part2)
