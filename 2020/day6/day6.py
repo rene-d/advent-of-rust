45f6e577fc3c8945952f178e68cf37e7d48f8520
rene-d <rene-d@users.noreply.github.com>
2023-12-13 09:02:55 +0100

    2020

---
+++ 2020/day6/day6.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# https://adventofcode.com/2020/day/6
+
+from pathlib import Path
+from copy import deepcopy
+from collections import defaultdict, deque, namedtuple
+import sys, re, math, itertools, time
+from functools import reduce
+import re
+
+verbose = "-v" in sys.argv
+if verbose:
+    sys.argv.remove("-v")
+filename = ("test.txt" if sys.argv[1] == "-t" else sys.argv[1]) if len(sys.argv) > 1 else "input.txt"
+data = Path(filename).read_text().strip()
+lines = data.splitlines()
+
+result = 0
+for group in data.split("\n\n"):
+    group = group.replace("\n", "")
+    result += len(set(group))
+print(result)
+
+
+result = 0
+for group in data.split("\n\n"):
+    yes = None
+    for person in group.splitlines():
+        if yes is None:
+            yes = set(person)
+        else:
+            yes.intersection_update(set(person))
+    result += len(set(yes))
+print(result)
---
+++ 2020/day8/day8.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+# https://adventofcode.com/2020/day/8
+
+from pathlib import Path
+from copy import deepcopy
+from collections import defaultdict, deque, namedtuple
+import sys, re, math, itertools, time
+from functools import reduce
+import re
+
+verbose = "-v" in sys.argv
+if verbose:
+    sys.argv.remove("-v")
+filename = ("test.txt" if sys.argv[1] == "-t" else sys.argv[1]) if len(sys.argv) > 1 else "input.txt"
+data = Path(filename).read_text().strip()
+lines = data.splitlines()
+
+
+def run(program):
+    acc = 0
+    ip = 0
+    visited = set()
+    while ip < len(program) and ip not in visited:
+        visited.add(ip)
+
+        instr = program[ip]
+        ip += 1
+
+        op, imm = instr.split(maxsplit=1)
+        if op == "nop":
+            pass
+        elif op == "acc":
+            acc += int(imm)
+        elif op == "jmp":
+            ip += int(imm) - 1
+        else:
+            raise ValueError(instr)
+
+    return acc, ip == len(program)
+
+
+# part 1
+acc, booted = run(lines)
+assert booted == False
+print(acc)
+
+
+# part 2
+for i in range(len(lines)):
+    backup = lines[i]
+
+    instr, imm = backup.split(maxsplit=1)
+    if instr == "nop":
+        lines[i] = f"jmp {imm}"
+    elif instr == "jmp":
+        lines[i] = f"nop {imm}"
+
+    acc, booted = run(lines)
+    if booted:
+        break
+
+    lines[i] = backup
+
+print(acc)
