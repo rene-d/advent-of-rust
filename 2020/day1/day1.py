af714a4fbc19fd531d0fe82133d3ba809542e05a
rene-d <rene-d@users.noreply.github.com>
2023-12-15 22:27:45 +0100

    2020

--- 2020/day1/day1.py
+++ 2020/day1/day1.py
@@ -1,12 +1,8 @@
 #!/usr/bin/env python3
 # https://adventofcode.com/2020/day/1
 
+import sys
 from pathlib import Path
-from copy import deepcopy
-from collections import defaultdict, deque, namedtuple
-import sys, re, math, itertools, time
-from functools import reduce
-import re
 
 filename = ("test.txt" if sys.argv[1] == "-t" else sys.argv[1]) if len(sys.argv) > 1 else "input.txt"
 data = Path(filename).read_text().strip()
