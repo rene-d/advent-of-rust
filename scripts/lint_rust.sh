e6323f44ec95d7714141f115035df56234ae100a
rene-d <rene-d@users.noreply.github.com>
2023-12-16 14:10:58 +0100

    scripts

--- scripts/lint_rust.sh
+++ scripts/lint_rust.sh
@@ -1,4 +1,37 @@
 #!/usr/bin/env bash
 
 cargo fmt
-cargo clippy -- -A clippy::pedantic -A clippy::all
+
+pedantic()
+{
+    local manifest=$1
+    local day=$2
+
+    if [[ -f $manifest ]] ; then
+
+        if ! cargo clippy --manifest-path $manifest -- -D clippy::pedantic 2>&1 | grep -qE '^error:|^warning:' ; then
+            echo -e "Day $day is \033[1;35mMAX PEDANTIC\033[0m. Congratz 🤓"
+
+        elif ! cargo clippy --manifest-path $manifest -- -W clippy::pedantic 2>&1 | grep -qE '^error:|^warning:' ; then
+            echo -e "Day $day is \033[94mVERY PEDANTIC\033[0m. Good game 👍"
+
+        elif ! cargo clippy --manifest-path $manifest -- -A clippy::pedantic 2>&1 | grep -qE '^error:|^warning:' ; then
+            echo -e "Day $day is \033[92mPEDANTIC\033[0m. Well done 😃"
+
+        elif ! cargo clippy --manifest-path $manifest -- -A clippy::all 2>&1 | grep -qE '^error:|^warning:' ; then
+            echo -e "Day $day is \033[1;37mjust good enough\033[0m 😐"
+
+        else
+            echo -e "Day $day is \033[1;31mWTF\033[0m 🤬. Go to https://www.rust-lang.org/learn."
+        fi
+
+    fi
+}
+
+if [[ -f Cargo.toml ]] && grep -q "[[bin]]" Cargo.toml ; then
+    cargo clippy -- -W clippy::pedantic
+else
+    for day in {1..25} ; do
+        pedantic day$day/Cargo.toml $day
+    done
+fi
