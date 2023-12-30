a85b7006ad01cfb85cf39680f33edc21f9172eec
rene-d <rene-d@users.noreply.github.com>
2023-12-30 12:17:54 +0100

    2018

---
+++ 2018/day4/day4.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# https://adventofcode.com/2018/day/4
+
+import re
+import sys
+from collections import defaultdict
+from pathlib import Path
+
+verbose = "-v" in sys.argv
+if verbose:
+    sys.argv.remove("-v")
+self_tests = "-T" in sys.argv
+if self_tests:
+    sys.argv.remove("-T")
+filename = ("test.txt" if sys.argv[1] == "-t" else sys.argv[1]) if len(sys.argv) > 1 else "input.txt"
+data = Path(filename).read_text().strip()
+lines = data.splitlines()
+
+sleeping = defaultdict(lambda: [0] * 60)
+asleep = None
+guard = None
+for line in sorted(lines):
+    _, _, _, _, minute, event = re.match(r"^\[(\d+)-(\d+)-(\d+) (\d+):(\d+)\] (.+)$", line).groups()
+    minute = int(minute)
+    if event.startswith("Guard #"):
+        guard = int(event.removeprefix("Guard #").removesuffix(" begins shift"))
+    else:
+        if event == "falls asleep":
+            asleep = minute
+        elif event == "wakes up":
+            for i in range(asleep, minute):
+                sleeping[guard][i] += 1
+
+# part 1
+guard = max((sum(v), guard) for guard, v in sleeping.items())[1]
+n = max((m, n) for (n, m) in enumerate(sleeping[guard]))[1]
+part1 = guard * n
+print(part1)
+
+# part 2
+_, guard, minute = max(max((v[m], guard, m) for guard, v in sleeping.items()) for m in range(60))
+part2 = guard * minute
+print(part2)
+
+
+if filename == "test.txt":
+    assert part1 == 240
+    assert part2 == 4455
---
+++ 2018/day4/day4.rs
@@ -0,0 +1,124 @@
+//! [Day 4: Repose Record](https://adventofcode.com/2018/day/4)
+
+use clap::Parser;
+use std::collections::HashMap;
+
+#[derive(Parser)]
+struct Args {
+    /// Puzzle input
+    #[arg(default_value = "input.txt")]
+    path: String,
+}
+
+struct Puzzle {
+    /// Number of minutes asleep for each minute from 00:00 to 00:59 by guard ID
+    sleeping: HashMap<u32, [u32; 60]>,
+}
+
+impl Puzzle {
+    fn new() -> Puzzle {
+        Puzzle {
+            sleeping: HashMap::new(),
+        }
+    }
+
+    /// Get the puzzle input.
+    fn configure(&mut self, path: &str) {
+        let data = std::fs::read_to_string(path).unwrap();
+
+        let mut lines: Vec<_> = data.lines().collect();
+        lines.sort_unstable();
+
+        let mut asleep: u32 = 0;
+        let mut current_guard: u32 = 0;
+
+        for line in &lines {
+            let minute: u32 = line[15..17].parse().unwrap();
+
+            if let Some(guard) = line[19..].strip_prefix("Guard #") {
+                current_guard = guard
+                    .strip_suffix(" begins shift")
+                    .unwrap()
+                    .parse()
+                    .unwrap();
+            } else if &line[19..] == "falls asleep" {
+                asleep = minute;
+            } else if &line[19..] == "wakes up" {
+                let e = self.sleeping.entry(current_guard).or_insert([0; 60]);
+
+                for m in asleep..minute {
+                    e[m as usize] += 1;
+                }
+            }
+        }
+    }
+
+    /// Solve part one.
+    fn part1(&self) -> u32 {
+        let guard_most_asleep = self
+            .sleeping
+            .iter()
+            .map(|(guard, v)| (v.iter().sum::<u32>(), guard)) // sum the minutes spent a sleep
+            .max() // keep the highest value
+            .unwrap()
+            .1; // retrieve the id of the guard
+
+        let minute_asleep_most: u32 = self.sleeping[guard_most_asleep]
+            .iter()
+            .enumerate() // index=minute, value=minutes spent asleep
+            .max_by_key(|&(_, n)| n) // select the max value within the array
+            .unwrap()
+            .0 // get the minute between 0 and 59
+            .try_into()
+            .unwrap();
+
+        guard_most_asleep * minute_asleep_most
+    }
+
+    /// Solve part two.
+    fn part2(&self) -> u32 {
+        let ((_n, guard), n) = (0..60)
+            .map(|m| {
+                (
+                    self.sleeping
+                        .iter()
+                        .map(|(guard, v)| (v[m], guard))
+                        .max() // get guard ID who is most asleep at minute m
+                        .unwrap(),
+                    m,
+                )
+            })
+            .max_by_key(|&((n, _guard), _m)| n)
+            .unwrap();
+
+        guard * u32::try_from(n).unwrap()
+    }
+}
+
+fn main() {
+    let args = Args::parse();
+    let mut puzzle = Puzzle::new();
+    puzzle.configure(args.path.as_str());
+    println!("{}", puzzle.part1());
+    println!("{}", puzzle.part2());
+}
+
+/// Test from puzzle input
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test01() {
+        let mut puzzle = Puzzle::new();
+        puzzle.configure("test.txt");
+        assert_eq!(puzzle.part1(), 240);
+    }
+
+    #[test]
+    fn test02() {
+        let mut puzzle = Puzzle::new();
+        puzzle.configure("test.txt");
+        assert_eq!(puzzle.part2(), 4455);
+    }
+}
