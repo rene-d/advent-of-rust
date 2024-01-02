4fc05a77f006775ac41f764113e67377562a8c12
rene-d <rene-d@users.noreply.github.com>
2024-01-02 08:25:37 +0100

    2017

---
+++ 2017/day15/day15.rs
@@ -0,0 +1,106 @@
+//! [Day 15: Dueling Generators](https://adventofcode.com/2017/day/15)
+
+use clap::Parser;
+
+#[derive(Parser)]
+struct Args {
+    /// Puzzle input
+    #[arg(default_value = "input.txt")]
+    path: String,
+}
+
+struct Puzzle {
+    a: u64,
+    b: u64,
+}
+
+impl Puzzle {
+    fn new() -> Puzzle {
+        Puzzle { a: 0, b: 0 }
+    }
+
+    /// Get the puzzle input.
+    fn configure(&mut self, path: &str) {
+        let data = std::fs::read_to_string(path).unwrap();
+
+        for line in data.lines() {
+            if let Some(a) = line.strip_prefix("Generator A starts with ") {
+                self.a = a.parse().unwrap();
+            } else if let Some(b) = line.strip_prefix("Generator B starts with ") {
+                self.b = b.parse().unwrap();
+            }
+        }
+    }
+
+    /// Solve part one.
+    fn part1(&self) -> u32 {
+        let mut a: u64 = self.a;
+        let mut b: u64 = self.b;
+        let mut n = 0;
+        for _ in 0..40_000_000 {
+            a = a.wrapping_mul(16807) % 2147483647;
+            b = b.wrapping_mul(48271) % 2147483647;
+
+            if a & 0xffff == b & 0xffff {
+                n += 1;
+            }
+        }
+        n
+    }
+
+    /// Solve part two.
+    fn part2(&self) -> u32 {
+        let mut a: u64 = self.a;
+        let mut b: u64 = self.b;
+        let mut n = 0;
+        for _ in 0..5_000_000 {
+            loop {
+                a = a.wrapping_mul(16807) % 2147483647;
+                if a % 4 == 0 {
+                    break;
+                }
+            }
+            loop {
+                b = b.wrapping_mul(48271) % 2147483647;
+                if b % 8 == 0 {
+                    break;
+                }
+            }
+
+            if a & 0xffff == b & 0xffff {
+                n += 1;
+            }
+        }
+        n
+    }
+}
+
+fn main() {
+    let args = Args::parse();
+    let mut puzzle = Puzzle::new();
+    puzzle.configure(args.path.as_str());
+    println!("{}", puzzle.part1());
+    println!("{}", puzzle.part2());
+}
+
+/// Test from puzzle input
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test01() {
+        let mut puzzle = Puzzle::new();
+        puzzle.a = 65;
+        puzzle.b = 8921;
+        assert_eq!(puzzle.part1(), 588);
+    }
+
+    #[test]
+    fn test02() {
+        let mut puzzle = Puzzle::new();
+        puzzle.a = 65;
+        puzzle.b = 8921;
+        assert_eq!(puzzle.part2(), 309);
+    }
+}
---
+++ 2017/day16/day16.rs
@@ -0,0 +1,113 @@
+//! [Day 16: Permutation Promenade](https://adventofcode.com/2017/day/16)
+
+use clap::Parser;
+
+#[derive(Parser)]
+struct Args {
+    /// Puzzle input
+    #[arg(default_value = "input.txt")]
+    path: String,
+}
+
+struct Puzzle {
+    program: Vec<String>,
+}
+
+impl Puzzle {
+    fn new() -> Puzzle {
+        Puzzle { program: vec![] }
+    }
+
+    /// Get the puzzle input.
+    fn configure(&mut self, path: &str) {
+        let data = std::fs::read_to_string(path).unwrap();
+
+        self.program = data.trim().split(',').map(|s| s.to_string()).collect();
+    }
+
+    fn dance(&self, text: &mut [u8]) {
+        for dance in &self.program {
+            if let Some(s) = dance.strip_prefix('s') {
+                let s = s.parse().unwrap();
+                text.rotate_right(s);
+            } else if let Some(x) = dance.strip_prefix('x') {
+                let (a, b) = x.split_once('/').unwrap();
+                let a = a.parse().unwrap();
+                let b = b.parse().unwrap();
+
+                text.swap(a, b);
+            } else if let Some(p) = dance.strip_prefix('p') {
+                let (a, b) = p.split_once('/').unwrap();
+
+                let pos = |ch: u8| text.iter().position(|c| c == &ch).unwrap();
+
+                let a = pos(a.as_bytes()[0]);
+                let b = pos(b.as_bytes()[0]);
+
+                text.swap(a, b);
+            }
+        }
+    }
+
+    /// Solve part one.
+    fn part1(&self) -> String {
+        let mut text: Vec<u8> = "abcdefghijklmnop".bytes().collect();
+
+        self.dance(&mut text);
+
+        text.iter().map(|b| *b as char).collect::<String>()
+    }
+
+    /// Solve part two.
+    fn part2(&self) -> String {
+        let mut text: Vec<u8> = "abcdefghijklmnop".bytes().collect();
+        let initial = text.clone();
+
+        let mut seen = vec![];
+        seen.push(text.clone());
+
+        for i in 1.. {
+            self.dance(&mut text);
+
+            let ok = (0..16).all(|i| initial[i] == text[i]);
+
+            if ok {
+                return seen[1_000_000_000 % i]
+                    .iter()
+                    .map(|b| *b as char)
+                    .collect::<String>();
+            } else {
+                seen.push(text.clone());
+            }
+        }
+        unreachable!();
+    }
+}
+
+fn main() {
+    let args = Args::parse();
+    let mut puzzle = Puzzle::new();
+    puzzle.configure(args.path.as_str());
+    println!("{}", puzzle.part1());
+    println!("{}", puzzle.part2());
+}
+
+/// Test from puzzle input
+#[cfg(test)]
+mod test {
+    // use super::*;
+
+    // #[test]
+    // fn test01() {
+    //     let mut puzzle = Puzzle::new();
+    //     puzzle.configure("test.txt");
+    //     assert_eq!(puzzle.part1(), 0);
+    // }
+
+    // #[test]
+    // fn test02() {
+    //     let mut puzzle = Puzzle::new();
+    //     puzzle.configure("test.txt");
+    //     assert_eq!(puzzle.part2(), 0);
+    // }
+}
