a7632696ffe90217c8eef85a0d6dc94b72a3e5d7
rene-d <rene-d@users.noreply.github.com>
2024-01-04 00:10:15 +0100

    2017

---
+++ 2017/day22/day22.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python3
+# https://adventofcode.com/2017/day/22
+
+import sys
+from collections import defaultdict
+from pathlib import Path
+
+verbose = "-v" in sys.argv
+if verbose:
+    sys.argv.remove("-v")
+self_tests = "-T" in sys.argv
+if self_tests:
+    sys.argv.remove("-T")
+filename = ("test.txt" if sys.argv[1] == "-t" else sys.argv[1]) if len(sys.argv) > 1 else "input.txt"
+data = Path(filename).read_text().strip()
+lines = data.splitlines()
+
+nx = len(lines[0])
+ny = len(lines)
+
+
+MOVES = ((0, -1), (1, 0), (0, 1), (-1, 0))  # up/right/down/left
+
+
+# part 1
+
+infected = set()
+for y, line in enumerate(lines):
+    for x, c in enumerate(line):
+        if c == "#":
+            infected.add((x, y))
+
+x, y = nx // 2, ny // 2  # middle of the map
+d = 0  # facing up
+
+infections1 = 0
+for _ in range(10000):
+    if (x, y) in infected:
+        d = (d + 1) % 4  # turn right
+        infected.discard((x, y))  # infect the node
+    else:
+        d = (d + 3) % 4  # turn left
+        infected.add((x, y))  # infect the node
+        infections1 += 1
+
+    dx, dy = MOVES[d]  # move
+    x, y = x + dx, y + dy
+
+print(infections1)
+
+
+# part 2
+
+CLEAN = 0
+WEAKENED = 1
+INFECTED = 2
+FLAGGED = 3
+
+nodes = defaultdict(lambda: CLEAN)
+for y, line in enumerate(lines):
+    for x, c in enumerate(line):
+        if c == "#":
+            nodes[x, y] = INFECTED
+
+x, y = nx // 2, ny // 2  # middle of the map
+d = 0  # facing up
+
+infections2 = 0
+for _ in range(10_000_000):
+    node = nodes[x, y]
+
+    if node == CLEAN:
+        d = (d + 3) % 4  # turn left
+        node = WEAKENED
+
+    elif node == WEAKENED:
+        node = INFECTED
+        # do not turn
+        infections2 += 1
+
+    elif node == INFECTED:
+        d = (d + 1) % 4  # turn right
+        node = FLAGGED
+
+    elif node == FLAGGED:
+        node = CLEAN
+        d = (d + 2) % 4  # go back
+
+    else:
+        assert False
+
+    nodes[x, y] = node
+
+    dx, dy = MOVES[d]  # move
+    x, y = x + dx, y + dy
+
+print(infections2)
+
+
+# test
+if filename == "test":
+    assert infections1 == 5587
+    assert infections2 == 2511944
---
+++ 2017/day25/day25.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+# https://adventofcode.com/2017/day/25
+
+import re
+import sys
+from pathlib import Path
+
+
+class Blueprint:
+    def __init__(self):
+        self.write_if = [None, None]
+        self.move_if = [None, None]
+        self.state_if = [None, None]
+
+
+verbose = "-v" in sys.argv
+if verbose:
+    sys.argv.remove("-v")
+self_tests = "-T" in sys.argv
+if self_tests:
+    sys.argv.remove("-T")
+filename = ("test.txt" if sys.argv[1] == "-t" else sys.argv[1]) if len(sys.argv) > 1 else "input.txt"
+data = Path(filename).read_text().strip()
+
+
+blueprints = {}
+state = None
+steps = None
+current, condition = None, None
+for line in data.splitlines():
+    line = line.strip()
+
+    if not line:
+        current, condition = None, None
+
+    elif m := re.match(r"^Begin in state (?P<state>\w)\.$", line):
+        state = m["state"]
+
+    elif m := re.match(r"^Perform a diagnostic checksum after (?P<steps>\d+) steps\.$", line):
+        steps = int(m["steps"])
+
+    elif m := re.match(r"^In state (\w):$", line):
+        current = Blueprint()
+        blueprints[m.group(1)] = current
+
+    elif line == "If the current value is 0:":
+        condition = 0
+
+    elif line == "If the current value is 1:":
+        condition = 1
+
+    elif line == "- Move one slot to the left.":
+        current.move_if[condition] = -1
+
+    elif line == "- Move one slot to the right.":
+        current.move_if[condition] = 1
+
+    elif line == "- Write the value 1.":
+        current.write_if[condition] = 1
+
+    elif line == "- Write the value 0.":
+        current.write_if[condition] = 0
+
+    elif m := re.match(r"^- Continue with state (\w).$", line):
+        current.state_if[condition] = m.group(1)
+
+
+tape = set()
+cursor = 0
+
+for _ in range(steps):
+    b = blueprints[state]
+
+    value = cursor in tape  # current value 1 / 0
+
+    if b.write_if[value] == 1:
+        tape.add(cursor)
+    else:
+        tape.discard(cursor)
+
+    cursor += b.move_if[value]
+
+    state = b.state_if[value]
+
+# part 1
+print(len(tape))
+
+# test
+if filename == "test.txt":
+    assert len(tape) == 3
