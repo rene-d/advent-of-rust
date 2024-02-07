3f891be9ee4f6956ea8de7eba77cbe6389be6933
rene-d <rene-d@users.noreply.github.com>
2024-02-07 09:18:02 +0100

    docker

--- scripts/allpython.sh
+++ scripts/allpython.sh
@@ -4,6 +4,8 @@ set -euo pipefail
 
 script_dir=$(realpath $(dirname $0))
 
+mkdir -p /opt/python
+
 i()
 {
     local v=$1
@@ -15,22 +17,24 @@ i()
     rm -rf /tmp/Python-$v /opt/python/Python-$v
     curl -sL $url | tar -C /tmp -xJ
     cd /tmp/Python-$v
-    ./configure --prefix=/opt/python/Python-$v --enable-optimizations --disable-test-modules
+    ./configure --prefix=/opt/python/Python-$v --enable-optimizations
     make -j$(nproc --ignore=1)
     make altinstall
     # /opt/python/Python-$v/bin/python$m -mensurepip
     cd /tmp
     rm -rf /tmp/Python-$v
 
-    $script_dir/runall.py --venv /opt/python/Python-$v/bin/python$m
+    if [ -f $script_dir/runall.py ] ; then
+        $script_dir/runall.py --venv /opt/python/Python-$v/bin/python$m
+    fi
 }
 
 a()
 {
     i 3.10.13
-    i 3.11.6
+    i 3.11.7
     i 3.12.1
-    # i 3.13.0a3
+    i 3.13.0a3
 }
 
 if [ ${1-} ]; then
